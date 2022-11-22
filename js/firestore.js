114af2e51080665d9bcc9629dd5950df1630102e
Willian Ribeiro <willihr10@gmail.com>
2022-11-22 13:58:08 -0300

    template dinamico membros atletica

--- js/firestore.js
+++ js/firestore.js
@@ -1,5 +1,14 @@
 const membrosCollection = firebase.firestore().collection('membros');
 membrosCollection.get().then((querySnapshot) => {
-    const membrosJson = querySnapshot.docs.map((doc) => doc.data());
-    console.log(membrosJson);
+    querySnapshot.docs.forEach((doc) => {
+        const membroJson = doc.data();
+
+        const template = document.querySelector("#membrosCardsTemplate");
+        const copy = template.content.cloneNode(true);
+        copy.querySelector('.name-dir .name').innerHTML = membroJson.nome;
+        copy.querySelector('.name-dir .nickname').innerHTML = membroJson.apelido;
+        copy.querySelector('.name-dir .description').innerHTML = membroJson.cargo;
+        template.parentNode.appendChild(copy);
+    });
+
 });
