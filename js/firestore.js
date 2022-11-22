234c988a2fef25d947e64bf97a847856f1e922e1
Willian Ribeiro <willihr10@gmail.com>
2022-11-22 14:10:06 -0300

    correção carrosel

--- js/firestore.js
+++ js/firestore.js
@@ -11,4 +11,58 @@ membrosCollection.get().then((querySnapshot) => {
         template.parentNode.appendChild(copy);
     });
 
+    var swiper = new Swiper(".slide-content", {
+        slidesPerView: 3,
+        spaceBetween: 125,
+        loop: true,
+        loopedSlides: 0,
+        loopedSlidesLimit: false,
+        centerSlide:'true',
+        fade:'true',
+        grabCursor:'true',
+        effect:'card',
+        loopFillGroupWithBlank: true,
+        centeredSlidesBounds:true,
+        watchSlidesProgress:true,
+        coverflowEffect:{
+          rotate:0,
+          stretch: 0,
+          depth:100,
+          modifier: 2,
+          slideShadows:true,
+          
+        },
+    
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          dynamicBullets: true,
+        },
+    
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+    
+        breakpoints:{
+            0: {
+                slidesPerView: 1,
+            },
+            520: {
+                slidesPerView: 2,
+            },
+            950: {
+                slidesPerView: 3,
+            }
+        },
+      });
+
+    VanillaTilt.init(document.querySelectorAll(".swiper-diretoria .swiper-slide"), {
+        max:20,
+        speed:400,
+        glare: true,
+        "max-glare": 0.3,
+        gyroscopeMaxAngleZ:0,
+        gyroscopeMinAngleZ:0,
+    });
 });
