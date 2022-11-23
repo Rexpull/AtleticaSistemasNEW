1ccdd989fc6df7253b396253e7cf457d641f1cf5
Willian Ribeiro <willihr10@gmail.com>
2022-11-23 16:37:30 -0300

    CRUD produtos ok

--- js/adminAtletica.js
+++ js/adminAtletica.js
@@ -1,3 +1,4 @@
+// CRUD Atlética
 
 const membrosCollection = firebase.firestore().collection('membros');
 const membrosPhotosStorage = firebase.storage().ref().child('membros');
@@ -5,14 +6,14 @@ const membrosPhotosStorage = firebase.storage().ref().child('membros');
 membrosCollection.get().then((querySnapshot) => {
     $('#tabelaMembrosAtletica tbody').html();
     querySnapshot.docs.forEach((doc) => {
-        const membroJson = doc.data();
+        const json = doc.data();
 
         const row = $('<tr/>');
-        row.append(`<td><img src="${membroJson.imagem}" alt="Foto"></td>`);
-        row.append(`<td>${membroJson.nome}</td>`);
-        row.append(`<td>${membroJson.apelido}</td>`);
-        row.append(`<td>${membroJson.cargo}</td>`);
-        row.append(`<td><a href="${membroJson.link}">${membroJson.link ? 'Link' : ''}</a></td>`);
+        row.append(`<td><img src="${json.imagem}" alt="Foto"></td>`);
+        row.append(`<td>${json.nome}</td>`);
+        row.append(`<td>${json.apelido}</td>`);
+        row.append(`<td>${json.cargo}</td>`);
+        row.append(`<td><a href="${json.link}">${json.link ? 'Link' : ''}</a></td>`);
         row.append(`<td>
             <a href="#addEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons"
                     data-toggle="tooltip" title="Editar">&#xE254;</i></a>
@@ -20,7 +21,7 @@ membrosCollection.get().then((querySnapshot) => {
                     class="material-icons" data-toggle="tooltip" title="Remover">&#xE872;</i></a>
         </td>`);
         row.data('id', doc.id);
-        row.data('data', membroJson);
+        row.data('data', json);
 
         $('#tabelaMembrosAtletica tbody').append(row);
     });
@@ -89,6 +90,94 @@ $(document).ready(() => {
         window.location.reload();
     });
 })
+
+
+// CRUD Produtos
+
+const produtosCollection = firebase.firestore().collection('produtos');
+const produtosPhotosStorage = firebase.storage().ref().child('produtos');
+
+produtosCollection.get().then((querySnapshot) => {
+    $('#tabelaProdutos tbody').html();
+    querySnapshot.docs.forEach((doc) => {
+        const json = doc.data();
+
+        const row = $('<tr/>');
+        row.append(`<td><img src="${json.imagem}" alt="Foto"></td>`);
+        row.append(`<td>${json.nome}</td>`);
+        row.append(`<td>${json.preco}</td>`);
+        row.append(`<td>
+            <a href="#addProductModal" class="edit" data-toggle="modal"><i class="material-icons"
+                    data-toggle="tooltip" title="Editar">&#xE254;</i></a>
+            <a href="#deleteProductModal" class="delete" data-toggle="modal"><i
+                    class="material-icons" data-toggle="tooltip" title="Remover">&#xE872;</i></a>
+        </td>`);
+        row.data('id', doc.id);
+        row.data('data', json);
+
+        $('#tabelaProdutos tbody').append(row);
+    });
+    $('#tabelaProdutos tbody .edit').click(function() {
+        const id = $(this).closest('tr').data('id');
+        const data = $(this).closest('tr').data('data');
+        $('#addProductModal').data('id', id);
+        $('#addProductModal').data('imagem', data.imagem);
+        $('#addProductModal #formNome').val(data.nome);
+        $('#addProductModal #formPreco').val(data.preco);
+    });
+    $('#tabelaProdutos tbody .delete').click(function() {
+        const id = $(this).closest('tr').data('id');
+        $('#deleteProductModal').data('id', id);
+    });
+});
+
+$(document).ready(() => {
+    $('#addProductBtn').click(() => {
+        $('#addProductModal').data('id', null);
+        $('#addProductModal').data('imagem', null);
+        $('#addProductModal #formNome').val(null);
+        $('#addProductModal #formPreco').val(null);
+    });
+
+    $('#addProductModal #formPhoto').change(e => {
+        $('#addProductModal input[type=submit]').attr('disabled', true);
+        const file = e.target.files[0];
+        produtosPhotosStorage.child(uuidv4()).put(file).then(snapshot => {
+            snapshot.ref.getDownloadURL().then(url => {
+                $('#addProductModal').data('imagem', url);
+                $('#addProductModal input[type=submit]').attr('disabled', null);
+            })
+        });
+    });
+
+    $('#addProductModal form').submit(async e => {
+        e.preventDefault();
+        $('#addProductModal').modal('hide');
+        const id = $('#addProductModal').data('id');
+        const data = {
+            nome: $('#addProductModal #formNome').val() || '',
+            preco: $('#addProductModal #formPreco').val() || '',
+            imagem: $('#addProductModal').data('imagem') || '',
+        };
+        if (id)
+            await produtosCollection.doc(id).set(data);
+        else
+            await produtosCollection.add(data);
+        window.location.reload();
+    });
+
+    $('#deleteProductModal form').submit(async e => {
+        e.preventDefault();
+        $('#deleteProductModal').modal('hide');
+        const id = $('#deleteProductModal').data('id');
+        if (id) {
+            await produtosCollection.doc(id).delete();
+        }
+        window.location.reload();
+    });
+})
+
+// Utils
 
 function uuidv4() {
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
