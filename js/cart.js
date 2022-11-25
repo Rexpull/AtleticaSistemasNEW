3a19f8ab3b0c427fded279f64c25ae2e9a0ce9ae
Willian Ribeiro <willihr10@gmail.com>
2022-11-25 17:47:30 -0300

    hgjhgh

--- js/cart.js
+++ js/cart.js
@@ -201,3 +201,33 @@ Um momento que já entraremos em contato com você!`;
   localStorage.setItem("carrinho", null);
   $("#carrinhoModal").modal("hide");
 }
+
+function mascaraTelefone(event) {
+  let tecla = event.key;
+  let telefone = event.target.value.replace(/\D+/g, "");
+
+  if (/^[0-9]$/i.test(tecla)) {
+    telefone = telefone + tecla;
+    let tamanho = telefone.length;
+
+    if (tamanho >= 12) {
+      return false;
+    }
+
+    if (tamanho > 10) {
+      telefone = telefone.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+    } else if (tamanho > 5) {
+      telefone = telefone.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+    } else if (tamanho > 2) {
+      telefone = telefone.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+    } else {
+      telefone = telefone.replace(/^(\d*)/, "($1");
+    }
+
+    event.target.value = telefone;
+  }
+
+  if (!["Backspace", "Delete"].includes(tecla)) {
+    return false;
+  }
+}
